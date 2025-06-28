4340dbe989ccd721c1d71c62ac3cf6d80c78e352
saneshps <pssanesh09@gmail.com>
2025-06-28 16:24:57 +0530

    last

--- js/history.js
+++ js/history.js
@@ -4,37 +4,21 @@ var swiper = new Swiper(".history", {
     delay: 2500,
     disableOnInteraction: false,
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-     640: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
     768: {
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 0,
     },
     1024: {
-      slidesPerView: 5,
+      slidesPerView: 4,
       spaceBetween: 20,
     },
     1366: {
-      slidesPerView: 6,
+      slidesPerView: 5,
       spaceBetween: 0,
     },
     1920: {
-      slidesPerView: 4,
+      slidesPerView: 5,
       spaceBetween: 10,
     },
   },
