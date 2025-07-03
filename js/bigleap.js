eda8b0df7b3c82a5682c74159d61d49e0ffc4498
snehacmyk <sneharamesan0@gmail.com>
2025-07-03 16:56:39 +0530

    index-page-updated

---
+++ js/bigleap.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".bigleap", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //     delay: 7500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 80,
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        1366: {
+            slidesPerView: 1,
+            spaceBetween: 60,
+        },
+        1920: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+});
